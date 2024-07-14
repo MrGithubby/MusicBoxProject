24a67802116d4004ab17d4b38cddfbdb6add87a8
MrGithubby <74692936+MrGithubby@users.noreply.github.com>
2024-07-14 11:45:39 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,20 +62,20 @@ const getMusicPlaylistbyGenre = function () {
     console.log(genre);
     const apiUrl = `http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=${genre}&api_key=a44d846982283933b1ebb0aacdef6e3b&format=json`;
    
-    //fetch request
-    // function jsonp(apiURL, callback) {
-    //   const script = document.createElement('script');
-    //   const callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
-    //   window[callbackName] = function(data) {
-    //     delete window[callbackName];
-    //     document.body.removeChild(script);
-    //     callback(data);
-    //   };
-    //   script.src = apiUrl + (apiUrl.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
-    //   document.body.appendChild(script);
-    // }
+    fetch request
+    function jsonp(apiURL, callback) {
+      const script = document.createElement('script');
+      const callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+      window[callbackName] = function(data) {
+        delete window[callbackName];
+        document.body.removeChild(script);
+        callback(data);
+      };
+      script.src = apiUrl + (apiUrl.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+      document.body.appendChild(script);
+    }
     
-    jsonp('http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=${artist}&api_key=a44d846982283933b1ebb0aacdef6e3b&format=json', function(data) {
+    jsonp('http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=${genre}&api_key=a44d846982283933b1ebb0aacdef6e3b&format=json', function(data) {
       const trackArray = data.tracks.track;
       function pickRandomTrack(arr) {
         let randomIndex = Math.floor(Math.random() * trackArray.length);
